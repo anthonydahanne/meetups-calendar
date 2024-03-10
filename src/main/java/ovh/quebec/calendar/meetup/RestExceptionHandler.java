51dc8f479a69be0766f28d0f23ac38507e96c4c7
Anthony Dahanne <anthony.dahanne@gmail.com>
2024-03-09 23:11:03 -0500

    Switch to Meetup Calendar * and deploy to fly

---
+++ src/main/java/ovh/quebec/calendar/meetup/RestExceptionHandler.java
@@ -0,0 +1,35 @@
+package ovh.quebec.calendar.meetup;
+
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.stream.Collectors;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+  
+   //other exception handlers
+  
+   @ExceptionHandler(ConstraintViolationException.class)
+   protected ResponseEntity<JsonError> handleEntityNotFound(
+           ConstraintViolationException ex) {
+
+       String problems = ex.getConstraintViolations()
+               .stream()
+               .map(constraintViolation -> constraintViolation.getMessage())
+               .collect(Collectors.joining( ";" ));
+       JsonError jsonError = new JsonError("ConstraintViolationException", problems);
+
+       return new ResponseEntity<>(jsonError, HttpStatus.BAD_REQUEST);
+   }
+}
+
+
+record JsonError(String problem, String detailedProblem){}
