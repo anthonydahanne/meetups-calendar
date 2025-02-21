2fa7acbecfbdf361a59e8153c95511c1e5e6bd15
Anthony Dahanne <anthony.dahanne@gmail.com>
2025-02-21 00:02:36 -0500

    Add hints for Jackson properties * to not fall into: https://stackoverflow.com/questions/74908739/spring-boot-3-native-image-with-jackson

--- src/main/java/dev/meetups/CalendarApiApplication.java
+++ src/main/java/dev/meetups/CalendarApiApplication.java
@@ -1,8 +1,15 @@
 package dev.meetups;
 
+import dev.meetups.cncf.model.CncfEvent;
+import dev.meetups.cncf.model.CncfResult;
+import dev.meetups.meetup.model.Group;
+import dev.meetups.meetup.model.MeetupEvent;
+import dev.meetups.meetup.model.Venue;
 import dev.meetups.model.Event;
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,6 +17,8 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
 @EnableScheduling
+@Configuration
+@RegisterReflectionForBinding({MeetupEvent.class, Group.class, Venue.class, Event.class, CncfEvent.class, CncfResult.class })
 public class CalendarApiApplication  implements RepositoryRestConfigurer {
 
 	public static void main(String[] args) {
@@ -20,7 +29,6 @@ public class CalendarApiApplication  implements RepositoryRestConfigurer {
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 		config.exposeIdsFor(Event.class);
 	}
-
 
 }
 
